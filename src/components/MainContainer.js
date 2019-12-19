5f2b9b32b9b98abff04a114c1a34aa4add74d3c2
ajedelmann <albert.edelmann@gmail.com>
2019-12-19 10:59:34 +0100

    communicate from child to parent

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -4,25 +4,40 @@ import ToDoContainer from "./ToDoContainer";
 import ToDonesContainer from "./ToDonesContainer";
 
 class MainContainer extends React.Component {
-  render() {
-    const data = [
-      { text: "Do your thing", status: false, id: 1407892 },
-      { text: "Be yourself", status: true, id: 1467892 },
-      { text: "Explain something", status: false, id: 5436436 },
-      { text: "Be a dog", status: true, id: 3436436 },
-      { text: "Bite a tree", status: false, id: 2436436 },
-      { text: "Plant a tree", status: false, id: 1436436 },
-      { text: "Save the world", status: true, id: 7436436 },
-      { text: "Achieve world piece", status: false, id: 4363438 }
-    ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { text: "Do your thing", status: false, id: 1407892 },
+        { text: "Be yourself", status: true, id: 1467892 },
+        { text: "Explain something", status: false, id: 5436436 },
+        { text: "Be a dog", status: true, id: 3436436 },
+        { text: "Bite a tree", status: false, id: 2436436 },
+        { text: "Plant a tree", status: false, id: 1436436 },
+        { text: "Save the world", status: true, id: 7436436 },
+        { text: "Achieve world piece", status: false, id: 4363438 }
+      ]
+    };
+  }
 
+  handleUpdate = () => {
+    const newData = this.state.items;
+    newData[0].text = "Mach dein Ding";
+    this.setState({ items: newData });
+  };
+
+  render() {
+    const data = this.state.items;
     const todos = data.filter(el => !el.status);
     const todones = data.filter(el => el.status);
 
     return (
       <main className="main-container">
         <FormContainer></FormContainer>
-        <ToDoContainer items={todos}></ToDoContainer>
+        <ToDoContainer
+          items={todos}
+          updateFromChild={this.handleUpdate}
+        ></ToDoContainer>
         <ToDonesContainer items={todones}></ToDonesContainer>
       </main>
     );
--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -2,6 +2,17 @@ import React from "react";
 import ToDoItem from "./ToDoItem";
 
 class ToDoContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // binding is necessary to make 'this' work in the callback
+    this.updateRandomly = this.updateRandomly.bind(this);
+  }
+
+  updateRandomly() {
+    this.props.updateFromChild();
+  }
+
   render() {
     const data = this.props.items;
     const todoItems = data.map(el => {
@@ -14,6 +25,9 @@ class ToDoContainer extends React.Component {
       <div className="todos-container">
         {data.length > 0 && (
           <div className="todos">
+            <button className="fake btn" onClick={this.updateRandomly}>
+              Random update
+            </button>
             <h5>TODOS</h5>
             {todoItems}
           </div>
