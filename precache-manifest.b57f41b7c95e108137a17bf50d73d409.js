35486c09774891d7ef71416a103804b254318c74
ajedelmann <albert.edelmann@gmail.com>
2020-01-30 09:03:22 +0100

    Updates

---
+++ precache-manifest.b57f41b7c95e108137a17bf50d73d409.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dcb296732b310965d14e532624b7d09",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "0750363e678f21b8ba82",
+    "url": "/todo-app/static/css/main.71615a35.chunk.css"
+  },
+  {
+    "revision": "f35089fde64ba05d388a",
+    "url": "/todo-app/static/js/2.bb1ae375.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todo-app/static/js/2.bb1ae375.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0750363e678f21b8ba82",
+    "url": "/todo-app/static/js/main.797d4ef0.chunk.js"
+  },
+  {
+    "revision": "04c0e12a0dd748a4bb99",
+    "url": "/todo-app/static/js/runtime-main.a5594779.js"
+  },
+  {
+    "revision": "e0c7b1d5fbc832d5f711b57dfbd74960",
+    "url": "/todo-app/static/media/friend.e0c7b1d5.png"
+  }
+]);
